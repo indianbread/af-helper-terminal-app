b9bb6aed1ba0ee5d03f41a89f7d24a5d96307cde
Nhan Dang <nhantdang@gmail.com>
2019-09-03 11:17:15 +1000

    moved files

--- class-methods.rb
+++ class-methods.rb
@@ -2,6 +2,8 @@ require "tty-prompt"
 
 def chadsvasccalc
 cvscore = 0
+
+#age prompt
     prompt = TTY::Prompt.new
     age = prompt.select("What is the patient\'s age?", %w(<65 65-74 >75))
     if age == "<65"
@@ -10,6 +12,16 @@ cvscore = 0
         cvscore += 1
     else
         cvscore += 2 
+    end
+    
+    #sex prompt
+    # prompt = TTY::Prompt.new
+    gender = prompt.select("What is the patient\'s gender?", %w(Female, Male))
+
+    if gender == "Female"
+        cvscore += 1
+    else
+        cvscore += 0
     end
  return cvscore
 end
