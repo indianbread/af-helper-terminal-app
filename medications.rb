abe573b5ced7f517b63e6a2b8505d903d7dc14b0
Nhan Dang <nhantdang@gmail.com>
2019-09-04 16:53:24 +1000

    drug table added

--- medications.rb
+++ medications.rb
@@ -1,33 +1,36 @@
 require 'terminal-table'
 require_relative 'patient-class.rb'
 require 'CSV'
+require 'colorize'
 
+def druglist
 headers = []
 drug_content = []
-# arr_1 = []
+
 File.open("druglist.csv","r").each_with_index do |line,index|
     if line.length > 0 #Ensure that the line is not empty
         if index == 0
             sub_arr = line.split(",")
             sub_arr.each do |field|
+                field.delete!("\r\n")
                 headers << field
             end
 
         else
-        drug_content << line
+            line.delete!("\r\n")
+            drug_content << line.split(",")
         end
     end
 end
-# csv_text = File.open('druglist.csv', 'r')
-# csv = CSV.parse(csv_text, :headers => true)
-# csv.each do |row|
-#     row_data = row.to_a
-#     arr_1 << row_data
-# end
 
-# p arr_1
-p headers
-p drug_content
+table = Terminal::Table.new :headings => headers, :rows => drug_content
 
-# table = Terminal::Table.new :headings => ['Word', 'Number'], :rows => rows
+puts "These are the recommended first line oral agents for anticoagulation. There is no preference as to which one is to be trialed first.".colorize(:blue ).colorize( :background => :white)
+puts
+puts table
+end
+
+
+
+
 
