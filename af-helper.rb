3142b836667807823186c4fc1321875773e8ead9
Nhan Dang <nhantdang@gmail.com>
2019-09-04 23:23:47 +1000

    moved argv note to main page

--- af-helper.rb
+++ af-helper.rb
@@ -2,6 +2,7 @@ require 'tty-prompt'
 require 'CSV'
 require 'tty-font'
 require 'colorize'
+require 'terminal-table'
 require_relative 'chadsvasccalc.rb'
 require_relative 'resultviewer.rb'
 require_relative 'medications.rb'
@@ -11,21 +12,32 @@ font = TTY::Font.new(:standard)
 
 puts font.write("AF-Helper")
 
-puts "Hi Doc! I am your cardiovascular companion, the atrial fibrillation helper! Select from the following options below:".colorize(:black ).colorize( :background => :yellow)
-puts 
-continue = "y"
+puts "Hi Doc! I am your cardiovascular companion, the atrial fibrillation helper!".colorize(:black ).colorize( :background => :yellow)
+puts "\r\r"
+notes_log = []
+def add_notes(notes_log)
+    notes_log << ARGV
+    end
 
-while continue == "y"
-    # begin
+add_notes(notes_log)
+
+table = Terminal::Table.new :title => "On-The-Fly Notes:", :rows => notes_log
+puts table
+puts "\r\r"
+puts "Select from the following options below:"
+# continue = "y"
+
+# while continue == "y"
+loop do
+    begin
         puts "1. CHADS-VASc calculator"
         puts "2. View CHADS-VASc results for a patient"
         puts "3. View anticoagulant drug list"
         puts "4. Add an anticoagulant medication for patient"
-        puts "5. View on-the-fly notes"
-        puts "6. Exit"
+        puts "5. Exit"
         puts
         print ">" 
-        selection = gets.strip.to_i
+        selection = STDIN.gets.chomp.to_i
         system('clear')
 
         if selection == 1
@@ -34,7 +46,7 @@ while continue == "y"
 
         elsif selection == 2
         puts "Enter the patient's name to view results"
-            patientname = gets.strip.downcase
+            patientname = STDIN.gets.chomp.downcase
             resultviewer(patientname)
 
         elsif selection == 3
@@ -45,33 +57,27 @@ while continue == "y"
             patientname = gets.strip.downcase
             patient = loadpatient(patientname)
             puts "Enter drug and dose"
-            medication = gets.strip
+            medication = STDIN.gets.chomp
             patient.medication=(medication)
             puts "Medication has been added to patient's record".colorize(:blue).colorize( :background => :white)
             puts
             patient.viewresult
 
         elsif selection == 5
-            notes_log = []
-            def add_notes(notes_log) 
-            notes_log << ARGV[0]
-            end 
-            
-            add_notes(notes_log)
-
-            
-
-        elsif selection == 6
         exit
         else
             puts 'Invalid selection!'  
         end
         puts "\nPress Y to continue or any other key to exit"
-        continue = gets.strip.downcase
-        system('clear')
-    # rescue => error_object
-    #     puts "Something went wrong! Return to menu? Y/N"
-    #     continue = gets.strip.downcase
-    # end
+        continue = STDIN.gets.chomp.downcase
+            if continue == "y"
+            system('clear')
+            else
+            break
+            end
+    rescue => error_object
+        puts "Something went wrong! Return to menu? Y/N"
+        continue = STDIN.gets.chomp.downcase
+    end
 end
 
